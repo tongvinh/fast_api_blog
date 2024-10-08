8eb4bfa7623d4cdb7ea5d19521791181211ddd64
Vinh Tong <tonggiavinh2010@gmail.com>
2024-10-08 09:53:08 +0700

    Initial project and setup database

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from database.database import engine
+from database import models
+from routers import post
+
+app = FastAPI()
+
+app.include_router(post.router)
+
+models.Base.metadata.create_all(engine)
